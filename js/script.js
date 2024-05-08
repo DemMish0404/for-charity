38c5642fa348ec184085434158b83dd5977bc4b1
Demid Mishukov <mishukov1337mail.ru@gmail.com>
2024-05-08 13:17:25 +0500

    another version of the site with another news block

--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
 
+
+//swiper междуу новостями
+const swiper = new Swiper('#swiper-between-the-news', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+
+
+
+
+
+
+// показывать при клике описание: 
+
+const blockWithSlides = document.getElementsByClassName('brand-new-news__block-with-slides')[0]
+
+blockWithSlides.addEventListener('click', (event)=>{
+  if(event.target.closest('.slide-brand-new-news')){
+     event.target.closest('.slide-brand-new-news').classList.toggle('active')
+  }
+})
+
+
 //? burger menu
 const body = document.querySelector('body')
 const headerContainer = document.getElementsByClassName('header__container')[0]
@@ -37,37 +79,43 @@ listWithNavItems.addEventListener('click', (event)=>{
 
 
 //? show all
-const btnShowAll = document.getElementsByClassName('news__btn')[0]
-const newsCardContainer = document.getElementById('news-card-container')
-const heightOfTheNewsArticle = document.getElementsByClassName('card-news')[0].offsetHeight;
-console.log(`высота карточки: ${heightOfTheNewsArticle}`)
-console.log(newsCardContainer)
+// const btnShowAll = document.getElementsByClassName('news__btn')[0]
+// const newsCardContainer = document.getElementById('news-card-container')
+// const heightOfTheNewsArticle = document.getElementsByClassName('card-news')[0].offsetHeight;
+// console.log(`высота карточки: ${heightOfTheNewsArticle}`)
+// console.log(newsCardContainer)
 
-newsCardContainer.style.maxHeight = `${heightOfTheNewsArticle + 6}px`
-btnShowAll.addEventListener('click', (event)=>{
+// newsCardContainer.style.maxHeight = `${heightOfTheNewsArticle + 6}px`
+// btnShowAll.addEventListener('click', (event)=>{
    
    
-   // P.S. чтобы при возвращении назад перекидывало на начало блока с новостями
-   if(btnShowAll.classList.contains('active')){
+//    // P.S. чтобы при возвращении назад перекидывало на начало блока с новостями
+//    if(btnShowAll.classList.contains('active')){
 
       
-      btnShowAll.href = '#news-title';
-   }
-   else{
-      btnShowAll.href = '#';
-   }
-   // P.S. чтобы при возвращении назад перекидывало на начало блока с новостями END
-   btnShowAll.classList.toggle('active')
-   newsCardContainer.classList.toggle('active')
-   if(btnShowAll.classList.contains('active')){
-      btnShowAll.innerText = "Скрыть новости"
-      newsCardContainer.style.maxHeight = 'none'
-   } else{
-      btnShowAll.innerText = 'Посмотреть все новости'
-      newsCardContainer.style.maxHeight = `${heightOfTheNewsArticle + 6}px`
-   }
+//       btnShowAll.href = '#news-title';
+//    }
+//    else{
+//       btnShowAll.href = '#';
+//    }
+//    // P.S. чтобы при возвращении назад перекидывало на начало блока с новостями END
+//    btnShowAll.classList.toggle('active')
+//    newsCardContainer.classList.toggle('active')
+//    if(btnShowAll.classList.contains('active')){
+//       btnShowAll.innerText = "Скрыть новости"
+//       newsCardContainer.style.maxHeight = 'none'
+//    } else{
+//       btnShowAll.innerText = 'Посмотреть все новости'
+//       newsCardContainer.style.maxHeight = `${heightOfTheNewsArticle + 6}px`
+//    }
 
    
-})
+// })
 
 //? show all end
+
+
+
+
+
+
